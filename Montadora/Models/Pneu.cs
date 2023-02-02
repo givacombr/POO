0a2ed08ce1587299c269565b314e03e905a7bd5a
Giv@s <101278555+givacombr@users.noreply.github.com>
2023-02-02 19:10:33 -0300

    criando novo projeto

--- Montadora/Models/Pneu.cs
+++ Montadora/Models/Pneu.cs
@@ -4,14 +4,14 @@
     {
         public int Aro { get; set; }
         public bool TWI { get; set; }
-        public string Cor { get; set; }
-        public string Marca { get; set; }
+        public string? Cor { get; set; }
+        public string? Marca { get; set; }
         public int VelocidadeMaxima { get; set; }
         public int VelocidadeAtual { get; set; }
         public bool Estepe { get; set; }
         public bool Estourado { get; set; }
         public bool Furado { get; set; }
-        public string Tipo { get; set; }
+        public string? Tipo { get; set; }
 
 
     }
